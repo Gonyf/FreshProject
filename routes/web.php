bf55403beed18a8e0e1d7eb4b3a177d0341e309a
Nicklas Schmidt Vejlgaard <qzw@live.dk>
2020-12-16 00:25:39 +0100

    routes -> getting querystring and wildcard

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    $name = request("name");
+
+    return view('test', [
+        "name" => $name
+    ]);
+});
+
+Route::get('/test/{id}', function ($id) {
+    $posts = [
+        "blog1" => "this is the first one",
+        "blog2" => "this is the second two"
+    ];
+
+    if (!array_key_exists($id, $posts)) {
+        abort(404, "that thing is not found");
+    }
+
+    return view('test', [
+        "name" => $posts[$id]
+    ]);
+});
